f4f0fbf66cb8bd79433b265a108745d5dc0523d5
sdornel <kai_dornel@hotmail.com>
2024-12-19 21:40:44 -0500

    create singleton data layer and updates to data structure

--- website/src/data/data.ts
+++ website/src/data/data.ts
@@ -1,1 +1,37 @@
-// data layer
+import TransliteratedLowercase from '../../../transliterated.lowercase.json';
+
+export interface Verse {
+    number: number;
+    text: string;
+}
+
+export interface Chapter {
+    number: number;
+    verses: Array<Verse>;
+}
+
+export interface Book {
+    book_name: string;
+    chapters: Array<Chapter>;
+}
+
+export interface TransliteratedData {
+    book: Book;
+}
+
+export default class Data {
+    private static instance: Data;
+
+    private constructor() {} // Prevent direct instantiation
+  
+    public static getInstance(): Data {
+      if (!Data.instance) {
+        Data.instance = new Data();
+      }
+      return Data.instance;
+    }
+  
+    get transliteratedLowercase() {
+      return TransliteratedLowercase;
+    }
+}
