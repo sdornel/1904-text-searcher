c8b265126bfd94505936b10140314ec0c818f1d1
sdornel <kai_dornel@hotmail.com>
2024-12-19 22:52:52 -0500

    explain strict mode

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// https://react.dev/reference/react/StrictMode always calls rendering functions twice in development
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
