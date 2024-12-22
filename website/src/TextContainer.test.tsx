abc92687ebb9400a4fa6e54b41e603ef79b0f341
sdornel <kai_dornel@hotmail.com>
2024-12-22 12:31:36 -0500

    increase unit test coverage

--- website/src/TextContainer.test.tsx
+++ website/src/TextContainer.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { TextContainer } from './TextContainer';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('./data/data', () => ({
     __esModule: true,
@@ -36,12 +37,12 @@ describe('TextContainer', () => {
     expect(screen.getByText('Found 1 instance(s)')).toBeInTheDocument();
   });
 
-  it('"e" includes diacriticals', () => {
+  it('"e" includes diacriticals when searching', () => {
     render(<TextContainer searchInput="er" selectedBook="" />);
     expect(screen.getByText('Found 1 instance(s)')).toBeInTheDocument();
   });
 
-  it('"o" includes diacriticals', () => {
+  it('"o" includes diacriticals when searching', () => {
     render(<TextContainer searchInput="rod" selectedBook="" />);
     expect(screen.getByText('Found 1 instance(s)')).toBeInTheDocument();
   });
@@ -50,4 +51,36 @@ describe('TextContainer', () => {
     render(<TextContainer searchInput="asdf" selectedBook="" />);
     expect(screen.queryByText('Found 0 instance(s)')).toBeNull();
   })
+
+  it('search query is not case sensitive', () => {
+    render(<TextContainer searchInput="ZZZ" selectedBook="" />);
+    expect(screen.queryByText('Found 0 instance(s)')).toBeNull();
+  })
+
+  it('copies search results to clipboard', async () => {
+    Object.defineProperty(navigator, 'clipboard', {
+      value: {
+        writeText: jest.fn(),
+      },
+    });
+    render(<TextContainer searchInput="Test Verse" selectedBook="mat" />);
+    await screen.findByText('Matthew');
+  
+    const copyButton = screen.getByText('Copy Search Results');
+    await userEvent.click(copyButton);
+  
+    await waitFor(() => {
+      expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+        expect.stringContaining('Book: Matthew')
+      );
+    });
+  });
+
+  it('displays no results when searchInput does not match', () => {
+    render(<TextContainer searchInput="randomtext" selectedBook="" />);
+
+    expect(screen.queryByText('Found')).not.toBeInTheDocument();
+    expect(screen.queryByText('Matthew')).not.toBeInTheDocument();
+    expect(screen.queryByText('Mark')).not.toBeInTheDocument();
+  });
 });
