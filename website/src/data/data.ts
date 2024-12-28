e964fa6c761d33a4999336e6da4998a2a6a2a3da
sdornel <kai_dornel@hotmail.com>
2024-12-27 22:25:19 -0500

    indentation

--- website/src/data/data.ts
+++ website/src/data/data.ts
@@ -1,46 +1,63 @@
 import TransliteratedLowercase from '../../converted-json-files/transliterated_lowercase.json';
-import GreekLowercase from '../../converted-json-files/greek_lowercase.json'
+import GreekLowercase from '../../converted-json-files/greek_lowercase.json';
+import Greek from '../../converted-json-files/greek.json';
 
 // Allow for declaration merging
 export interface Verse {
-    number: number;
-    text: string;
+  number: number;
+  text: string;
 }
 
 export interface Chapter {
-    number: number;
-    verses: Array<Verse>;
+  number: number;
+  verses: Array<Verse>;
 }
 
 export interface Book {
-    book_name: string;
-    chapters: Array<Chapter>;
+  book_name: string;
+  chapters: Array<Chapter>;
 }
 
 export type TransliteratedData = Array<Book>;
 
 export default class Data {
-    private static instance: Data;
-    chosenText: TransliteratedData = TransliteratedLowercase;
+  private static instance: Data;
+  chosenText: TransliteratedData = TransliteratedLowercase;
 
-    private constructor() {} // Prevent direct instantiation
-  
-    public static getInstance(): Data {
-      if (!Data.instance) {
-        Data.instance = new Data();
-      }
-      return Data.instance;
-    }
-  
-    get transliteratedLowercase(): TransliteratedData {
-      return TransliteratedLowercase;
-    }
+  private constructor() {} // Prevent direct instantiation
 
-    // get greekLowercase(): TransliteratedData {
-    //   return;
-    // }
-
-    get greekLowercase() : TransliteratedData {
-      return GreekLowercase;
+  public static getInstance(): Data {
+    if (!Data.instance) {
+      Data.instance = new Data();
     }
+    return Data.instance;
+  }
+
+  textSelector(text: string): void {
+    switch (text) {
+      case 'TransliteratedLowercase':
+        this.chosenText = TransliteratedLowercase;
+        break;
+      case 'GreekLowercase':
+        this.chosenText = GreekLowercase;
+        break;
+      case 'Greek':
+        this.chosenText = Greek;
+        break;
+      default:
+        throw new Error('Invalid text');
+    }
+  }
+
+  get transliteratedLowercase(): TransliteratedData {
+    return TransliteratedLowercase;
+  }
+
+  get greekLowercase(): TransliteratedData {
+    return GreekLowercase;
+  }
+
+  get greek() : TransliteratedData {
+    return Greek;
+  }
 }
