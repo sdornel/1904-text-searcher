78540ac02deb6e729df80ae77f9b8d99d8b3de88
sdornel <kai_dornel@hotmail.com>
2024-12-27 21:52:39 -0500

    text select

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,28 +1,45 @@
 import { useState } from 'react';
-import './App.css'
 import { Search } from './Search.tsx';
 import { TextContainer } from './TextContainer.tsx';
+import BookList from './BookList.tsx';
+import './index.css';
+import { TextSelect } from './TextSelect.tsx';
 
 function App() {
   console.log('renderApp');
+  const [selectedText, setSelectedText] = useState('');
   const [searchInput, setSearchInput] = useState('');
-  const [selectedBook, setSelectedBook] = useState('');
+  const [selectedBooks, setSelectedBooks] = useState<string[]>([]);
+
+  const handleTextSelect = (text: string): void => {
+    setSelectedText(text);
+  };
 
   const handleSearch = (input: string): void => {
     setSearchInput(input);
   };
 
-  const handleBookChange = (book: string): void => {
-    setSelectedBook(book);
+  const handleBookSelect = (book: string): void => {
+    setSelectedBooks((prev) => 
+      prev.includes(book) ? prev.filter((b) => b !== book) : [...prev, book]
+    );
   };
 
   return (
-    <>
-      <Search handleSearch={handleSearch} handleBookChange={handleBookChange}/>
-      <p>Search Query: {searchInput}</p>
-      <TextContainer searchInput={searchInput} selectedBook={selectedBook} />
-    </>
-  )
+    <div className="h-screen flex gap-4 p-4">
+      <div className="flex-1 flex flex-col border rounded-md shadow-md overflow-hidden">
+        <TextSelect handleTextSelect={handleTextSelect}/>
+        <Search handleSearch={handleSearch} />
+        <TextContainer searchInput={searchInput} selectedBooks={selectedBooks} />
+      </div>
+
+      <div className="w-1/4 flex flex-col border bg-gray-100 rounded-md shadow-md overflow-hidden">
+        <div className="flex-1 overflow-y-auto p-4">
+          <BookList onBookSelect={handleBookSelect} selectedBooks={selectedBooks} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
